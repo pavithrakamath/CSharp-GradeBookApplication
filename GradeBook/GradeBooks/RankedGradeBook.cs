40d7c15476e82b41e830e245e9127a7e4743c6c5
Pavithra Kamath <pavithrakamathgp@gmail.com>
2020-10-27 16:02:49 +0530

    Update RankedGradeBook.cs
    solution-3

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -19,6 +19,7 @@ namespace GradeBook.GradeBooks
             }
             int low =  (int)Math.Ceiling(Students.Count * 0.2);
             var orderedGrades = Students.OrderByDescending(x => x.AverageGrade).Select(x => x.AverageGrade).ToList();
+            
             if(averageGrade >= orderedGrades[low - 1]){
                 return 'A';
             }
@@ -33,10 +34,26 @@ namespace GradeBook.GradeBooks
             else if (averageGrade >= orderedGrades[(low * 4) - 1])
             {
                 return 'D';
-            }
-
-            
+            }            
             return 'F';
+        }
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+            base.CalculateStatistics();
+        }
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+            base.CalculateStudentStatistics(name);
         }
     }
 }
